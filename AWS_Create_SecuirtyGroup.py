cb4fa653a7b921ee67417c02ba87ec6da44e8602
pragyanandan <pragyanandan@gmail.com>
2017-11-09 17:35:21 +1300

    temp

---
+++ AWS_Create_SecuirtyGroup.py
@@ -0,0 +1,29 @@
+import boto3
+
+
+REGION_NAME = 'ap-southeast-2'
+AMI_ID = 'ami-1a668878'  # Amazon Linux AMI 2017.09.1 (HVM), SSD Volume Type - ami-1a668878
+
+ec2 = boto3.resource('ec2', region_name="ap-southeast-2")
+
+
+# create VPC
+vpc = ec2.create_vpc(CidrBlock='10.0.0.0/16')
+
+# we can assign a name to vpc, or any resource, by using tag
+vpc.create_tags(Tags=[{"Key": "Pragya", "Value": "CreatedbyCode"}])
+vpc.wait_until_available()
+
+print(vpc.id)
+
+
+# Create sec group
+sec_group = ec2.create_security_group(
+    GroupName='slice_0', Description='slice_0 sec group', VpcId=vpc.id)
+sec_group.authorize_ingress(
+    CidrIp='0.0.0.0/0',
+    IpProtocol='TCP',
+    FromPort=0,
+    ToPort=65535
+)
+print(sec_group.id)
--- aws.py
+++ aws.py
@@ -4,7 +4,7 @@ import boto3
 
 
 REGION_NAME = 'ap-southeast-2'
-AMI_ID = 'ami-aa1b34cf'  # Amazon Linux AMI 2017.09.1 (HVM), SSD Volume Type
+AMI_ID = 'ami-1a668878'  # Amazon Linux AMI 2017.09.1 (HVM), SSD Volume Type - ami-1a668878
 
 ec2 = boto3.resource('ec2', region_name="ap-southeast-2")
 
@@ -22,18 +22,16 @@ print(vpc.id)
 subnet1 = ec2.create_subnet(
     AvailabilityZone='ap-southeast-2a',
     CidrBlock='10.0.1.0/24',
-    VpcId=vpc.id,
+    VpcId=vpc.id,)
 
-)
 print(subnet1.id)
 
 # create subnet 2  - Private
 subnet2 = ec2.create_subnet(
     AvailabilityZone='ap-southeast-2b',
     CidrBlock='10.0.2.0/24',
-    VpcId=vpc.id,
+    VpcId=vpc.id,)
 
-)
 print(subnet2.id)
 
 
@@ -59,8 +57,20 @@ sec_group = ec2.create_security_group(
     GroupName='slice_0', Description='slice_0 sec group', VpcId=vpc.id)
 sec_group.authorize_ingress(
     CidrIp='0.0.0.0/0',
-    IpProtocol='icmp',
-    FromPort=-1,
-    ToPort=-1
+    IpProtocol='TCP',
+    FromPort=0,
+    ToPort=65535
 )
+
 print(sec_group.id)
+
+# find image id ami-aa1b34cf/ ap-souteast2
+# Create instance
+instances = ec2.create_instances(
+    ImageId=AMI_ID, InstanceType='t2.micro', MaxCount=1, MinCount=1,
+    NetworkInterfaces=[{'SubnetId': subnet1.id, 'DeviceIndex': 0, 'AssociatePublicIpAddress': True,
+                        'Groups': [sec_group.group_id]}])
+
+instances[0].wait_until_running()
+
+print(instances[0].id)
