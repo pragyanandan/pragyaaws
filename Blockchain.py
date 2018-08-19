cce7cebcbd21a3bbbdaebc5023f67bdc7683d53d
pragyanandan <pragyanandan@gmail.com>
2018-08-19 14:37:44 +1200

    temp

--- Blockchain.py
+++ Blockchain.py
@@ -1,9 +1,21 @@
 blockchain = [[1]]
 
-def add_value (transaction_amount):
-    blockchain.append([blockchain[-1],transaction_amount])
-    print(blockchain)
+def get_last_blockchain():
+    return blockchain[-1]
 
-add_value(78)
-add_value(89)
-add_value(90)
+
+def add_value (transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction,transaction_amount])
+
+def get_user_input():
+    return float(input ('your transaction amount '))
+
+
+for x in range(1,4):
+    tx_amount = get_user_input()
+    add_value(last_transaction=get_last_blockchain(), transaction_amount=tx_amount)
+
+
+print(tx_amount)
+
+print (blockchain)
